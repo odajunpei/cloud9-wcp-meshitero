f16f08801d028037c4453f6d2d9d4a8b6910910b
odajunpei <i.penju1022@gmail.com>
2021-08-12 08:09:30 +0000

    [Add]4終わり

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,8 +1,13 @@
 class PostImage < ApplicationRecord
-  
+
   belongs_to :user
   attachment :image #_idは含めない
   has_many :post_comments, dependent: :destroy
-  
-  
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
+
 end
